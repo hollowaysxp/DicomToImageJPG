6fb0d6454a9b2bdcbd949d0ecb8f0d341f4ed1ca
hollowaysxp <45345976+hollowaysxp@users.noreply.github.com>
2019-06-25 00:18:11 +0800

    Add files via upload

---
+++ DicomToJPG1/Form1.Designer.cs
@@ -0,0 +1,147 @@
+﻿namespace DicomToJPG1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("微軟正黑體", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.button1.Location = new System.Drawing.Point(577, 86);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(178, 69);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "輸出";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("微軟正黑體", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label1.Location = new System.Drawing.Point(28, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(105, 24);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "輸入資料夾";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("微軟正黑體", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.textBox1.Location = new System.Drawing.Point(139, 30);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(496, 33);
+            this.textBox1.TabIndex = 2;
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("微軟正黑體", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.button2.Location = new System.Drawing.Point(641, 31);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(118, 32);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "選擇路徑";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Font = new System.Drawing.Font("微軟正黑體", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "jpg",
+            "png",
+            "bmp",
+            "tiff",
+            "gif"});
+            this.comboBox1.Location = new System.Drawing.Point(139, 86);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 32);
+            this.comboBox1.TabIndex = 4;
+            this.comboBox1.Text = "jpg";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("微軟正黑體", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label2.Location = new System.Drawing.Point(47, 89);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(86, 24);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "輸出格式";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("微軟正黑體", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label3.Location = new System.Drawing.Point(47, 146);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(0, 37);
+            this.label3.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(767, 198);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Dcm轉Image Ver 1.00";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ DicomToJPG1/Form1.cs
@@ -0,0 +1,191 @@
+﻿using Dicom.Imaging;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace DicomToJPG1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            ////var image = new DicomImage(@"C:\dcm\input.dcm");
+            //ImageManager.SetImplementation(WinFormsImageManager.Instance);
+            //image.RenderImage().AsBitmap().Save(@"C;\dcm\inputtest.jpg");
+            ////Bitmap renderedImage = image.RenderImage().As<Bitmap>();
+            ////renderedImage.Save(@"C:\dcm\input123.jpg", ImageFormat.Jpeg);
+            //saveJpeg(@"C:\dcm\input123.jpg",renderedImage,95);
+
+            combostatic = comboBox1.Text;
+            text1static = textBox1.Text;
+            label3.Text = "轉檔中...";
+            fileCount = 0;
+           
+            Thread outputThread = new Thread(new ThreadStart(DcmToImage));
+            outputThread.Start();
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            folderBrowserDialog1.ShowDialog();
+            textBox1.Text = folderBrowserDialog1.SelectedPath;
+        }
+
+        public static string text1static = "";
+        private void DcmToImage()
+        {
+            if (text1static != "")
+            {
+                //outputString = "";
+                //outputStrbuilder.Clear();
+
+                List<string> lstr = DirSearch(text1static);
+
+                //MessageBox.Show("all:" +fileCount.ToString());
+                //label3.Text = "找到檔案總共:" + fileCount;
+                //progressBar1.Maximum = fileCount;
+                //timer1.Enabled = true;
+                //label3.Text = "找到檔案總共:" + fileCount + "\n" + "輸出CSV中...";
+
+                if (!Directory.Exists(text1static + @"\Output")) {
+                    Directory.CreateDirectory(text1static + @"\Output");
+                }
+
+                for (int i = 0; i < lstr.Count; i++)
+                {
+                    if (File.Exists(lstr[i]))
+                    {
+                        var image = new DicomImage(lstr[i]);
+                        //ImageManager.SetImplementation(WinFormsImageManager.Instance);
+                        //image.RenderImage().AsBitmap().Save(@"C;\dcm\inputtest.jpg");
+                        Bitmap renderedImage = image.RenderImage().As<Bitmap>();
+                        //String outputFileStr = lstr[i].Replace("dcm","jpg");
+                        //MessageBox.Show(Path.GetFileNameWithoutExtension(lstr[i]) + ".jpg");
+                        if (combostatic == "jpg")
+                        {
+                            renderedImage.Save(text1static + @"\Output\" + Path.GetFileNameWithoutExtension(lstr[i]) + "." + combostatic, ImageFormat.Jpeg);
+                        }
+                        else if (combostatic == "png")
+                        {
+                            renderedImage.Save(text1static + @"\Output\" + Path.GetFileNameWithoutExtension(lstr[i]) + "." + combostatic, ImageFormat.Png);
+                        }
+                        else if (combostatic == "bmp")
+                        {
+                            renderedImage.Save(text1static + @"\Output\" + Path.GetFileNameWithoutExtension(lstr[i]) + "." + combostatic, ImageFormat.Bmp);
+                        }
+                        else if (combostatic == "tiff")
+                        {
+                            renderedImage.Save(text1static + @"\Output\" + Path.GetFileNameWithoutExtension(lstr[i]) + "." + combostatic, ImageFormat.Tiff);
+                        }
+                        else if (combostatic == "gif")
+                        {
+                            renderedImage.Save(text1static + @"\Output\" + Path.GetFileNameWithoutExtension(lstr[i]) + "." + combostatic, ImageFormat.Gif);
+                        }
+                    }
+
+                    
+                }
+                //MessageBox.Show("轉檔完成");
+                ChangeLb(label3,"轉檔完成");
+                System.Diagnostics.Process.Start("explorer.exe", "\""+ text1static + @"\Output"+"\""); 
+            }
+            else
+            {
+                MessageBox.Show("輸入資料夾不能為空的");
+            }
+        }
+
+        private delegate void lbCallBack(Label label, String value);
+        private void ChangeLb(Label label, String value)
+        {
+            if (label.InvokeRequired)
+            {
+                lbCallBack d = new lbCallBack(ChangeLb);
+                this.Invoke(d, new object[]
+                {
+            label,
+            value
+                });
+            }
+            else
+            {
+                label.Text = value;
+            }
+        }
+
+
+        private static string combostatic = "";
+
+        private static int fileCount = 0;
+        private static List<String> DirSearch(string sDir)
+        {
+            List<String> files = new List<String>();
+            try
+            {
+                foreach (string f in Directory.GetFiles(sDir, "*." + "dcm")) //ext name .dcm
+                {
+                    files.Add(f);
+                    fileCount++;
+                }
+                foreach (string d in Directory.GetDirectories(sDir))
+                {
+                    files.AddRange(DirSearch(d));
+                }
+            }
+            catch (System.Exception excpt)
+            {
+                MessageBox.Show(excpt.Message);
+            }
+
+            return files;
+        }
+
+     
+
+
+        /*
+        private void saveJpeg(string path, Bitmap img, long quality)
+        {
+            // Encoder parameter for image quality
+            EncoderParameter qualityParam = new EncoderParameter(System.Drawing.Imaging.Encoder.Quality, quality);
+
+            // Jpeg image codec
+            ImageCodecInfo jpegCodec = this.getEncoderInfo("image /jpeg");
+            if (jpegCodec == null)
+                return;
+
+            EncoderParameters encoderParams = new EncoderParameters(1);
+            encoderParams.Param[0] = qualityParam;
+            MessageBox.Show("save");
+            img.Save(path, jpegCodec, encoderParams);
+        }
+
+        private ImageCodecInfo getEncoderInfo(string mimeType)
+        {
+            // Get image codecs for all image formats
+            ImageCodecInfo[] codecs = ImageCodecInfo.GetImageEncoders();
+
+            // Find the correct image codec
+            for (int i = 0; i < codecs.Length; i++)
+                if (codecs[i].MimeType == mimeType)
+                    return codecs[i];
+            return null;
+        }
+        */
+    }
+}
